c976bb1fb2b2ac92afbc22b559fa092ae8d93d60
RaphaelKono <raphaelkonopatzki@web.de>
2022-12-10 11:14:29 +0100

    firebase deploy

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+      projectId: 'ringoffire-5d1ce',
+      appId: '1:868057952977:web:f50f1d1ddcb10cee9e0bf0',
+      storageBucket: 'ringoffire-5d1ce.appspot.com',
+      apiKey: 'AIzaSyDdmxgY4zMliqgMGyz881Pks8q58lE3f-8',
+      authDomain: 'ringoffire-5d1ce.firebaseapp.com',
+      messagingSenderId: '868057952977',
+    },
+      production: true
+    };
